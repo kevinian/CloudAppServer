ad703fda4eee236a1fe6d89e4580f632e6f0a13b
kevinian <kevinpaul_boa@hotmail.com>
2013-08-25 15:44:37 +0200

    test push

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,6 +1,7 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
+	console.log('New coming request');
 	console.log(req.headers);
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end('Hello World\n');
