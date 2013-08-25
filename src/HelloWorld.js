0cfe95451d1580158ed8933d206477ce105772f5
kevinian <kevinpaul_boa@hotmail.com>
2013-08-25 15:47:28 +0200

    test bug

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -3,6 +3,7 @@ var http = require('http');
 http.createServer(function (req, res) {
 	console.log('New coming request');
 	console.log(req.headers);
+	console.log(req.url);
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end('Hello World\n');
 }).listen(8888);
