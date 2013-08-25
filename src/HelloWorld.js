239f2f0370f700bc2fa1ccb3f458a5aa20bdbf6c
kevinian <kevinpaul_boa@hotmail.com>
2013-08-25 15:50:47 +0200

    favicon

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -2,6 +2,14 @@ var http = require('http');
 
 http.createServer(function (req, res) {
 	console.log('New coming request');
+	
+	if (req.url === '/favicon.ico') {
+		res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+		res.end();
+	    console.log('favicon requested');
+	    return;
+	}
+	
 	console.log(req.headers);
 	console.log(req.url);
 	res.writeHead(200, {'Content-Type': 'text/plain'});
