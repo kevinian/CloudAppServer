0e4e28cf4d519258e89c0a48d402d76dc0969539
kevinian <kevinpaul_boa@hotmail.com>
2013-08-26 12:23:01 +0200

    bump

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,7 +1,9 @@
 var http = require('http');
+var express = require('express');
 
-http.createServer(function (req, res) {
-	
+var app = express();
+
+app.get('/', function(req, res){
 	if (req.url === '/favicon.ico') {
 		res.writeHead(200, {'Content-Type': 'image/x-icon'} );
 		res.end();
@@ -11,8 +13,29 @@ http.createServer(function (req, res) {
 	
 	console.log('New coming request');
 	console.log('Request URL: ' + req.url);
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World\n');
-}).listen(8888);
+	res.send('Hello World');
+//	res.writeHead(200, {'Content-Type': 'text/plain'});
+//	res.end('Hello World\n');
+  
+});
 
-console.log('Server running at http://127.0.0.1:8888/');
+app.listen(8888);
+console.log('Express started on port 8888');
+
+
+//http.createServer(function (req, res) {
+//	
+//	if (req.url === '/favicon.ico') {
+//		res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+//		res.end();
+//	    console.log('favicon requested');
+//	    return;
+//	}
+//	
+//	console.log('New coming request');
+//	console.log('Request URL: ' + req.url);
+//	res.writeHead(200, {'Content-Type': 'text/plain'});
+//	res.end('Hello World\n');
+//}).listen(8888);
+//
+//console.log('Server running at http://127.0.0.1:8888/');
