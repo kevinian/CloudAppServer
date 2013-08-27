6daf5d92fc92cefd296cd35852434c4f0b1448f1
kevinian <kevinpaul_boa@hotmail.com>
2013-08-28 00:15:44 +0200

    bump

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,51 +1,33 @@
 var http = require('http');
 var express = require('express');
-//var connect = require('connect');
 
 var app = express();
 
 app.use(express.favicon())
    .use(express.logger('dev'))
-   .use(express.bodyParser());
-
-app.use(function (req, res, next) {
-    console.log("first middle ware");                                                                                                             
-    next();
-});
+   .use(express.bodyParser())
+   .use(function (req, res, next) {
+	   console.log("cors");   
+	   next();
+    })
+   .use(function (req, res, next) {
+	   console.log("compress");   
+	   next();
+    })
+    .use(function (req, res, next) {
+	   console.log("robots.txt");   
+	   next();
+    })
+    .use(function (req, res, next) {
+	   console.log("sitemap.xml");   
+	   next();
+    });
 
 app.get('/', function(req, res){
-//	if (req.url === '/favicon.ico') {
-//		res.writeHead(200, {'Content-Type': 'image/x-icon'} );
-//		res.end();
-//	    console.log('favicon requested');
-//	    return;
-//	}
-	
 	console.log('New coming request');
 	console.log('Request URL: ' + req.url);
-	res.send('Hello World');
-//	res.writeHead(200, {'Content-Type': 'text/plain'});
-//	res.end('Hello World\n');
-  
+	res.json({ message: 'Hello World' });
 });
 
 app.listen(8888);
 console.log('Express started on port 8888');
-
-
-//http.createServer(function (req, res) {
-//	
-//	if (req.url === '/favicon.ico') {
-//		res.writeHead(200, {'Content-Type': 'image/x-icon'} );
-//		res.end();
-//	    console.log('favicon requested');
-//	    return;
-//	}
-//	
-//	console.log('New coming request');
-//	console.log('Request URL: ' + req.url);
-//	res.writeHead(200, {'Content-Type': 'text/plain'});
-//	res.end('Hello World\n');
-//}).listen(8888);
-//
-//console.log('Server running at http://127.0.0.1:8888/');
