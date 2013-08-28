a746de92f97a9f625e1643d3278c719aa7d2e152
kevinian <kevinpaul_boa@hotmail.com>
2013-08-28 22:58:26 +0200

    bump

--- src/server.js
+++ src/server.js
@@ -7,20 +7,21 @@ var negotiator = require('./handler/negotiator/contentNegotiator');
 var robots_txt = require('./handler/robots/robots_txt');
 var sitemap_xml = require('./handler/sitemap/sitemap_xml');
 
-var app = express()
-  .use(express.favicon())
-  .use(express.logger('dev'))
-  .use(negotiator())
-  .use(express.query())
-  .use(express.bodyParser())
-  .use(cors())
-  .use(robots_txt())
-  .use(sitemap_xml())
-  // compress works not, since res.json is not overridden by compress middleware
-  // TODO make sure res.headers['content-encoding'] is set correctly
-  .use(app.router)
-  .use(express.compress())
-  .use(express.static(path.join(__dirname, '../apps/e-shop/public')));
+var app = express();
+
+app.use(express.favicon())
+   .use(express.logger('dev'))
+   .use(negotiator())
+   .use(express.query())
+   .use(express.bodyParser())
+   .use(cors())
+   .use(robots_txt())
+   .use(sitemap_xml())
+   // compress works not, since res.json is not overridden by compress middleware
+   // TODO make sure res.headers['content-encoding'] is set correctly
+   .use(app.router)
+   .use(express.compress())
+   .use(express.static(path.join(__dirname, '../apps/e-shop/public')));
 
 app.get('/hello', function(req, res) {
   console.log('New coming request');
