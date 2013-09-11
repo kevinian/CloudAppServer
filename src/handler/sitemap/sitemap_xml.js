b0e6da76303364b76e195ca1b4f1b954f2314a74
kevinian <kevinpaul_boa@hotmail.com>
2013-09-11 21:29:59 +0200

    Update sitemap_xml.js

--- src/handler/sitemap/sitemap_xml.js
+++ src/handler/sitemap/sitemap_xml.js
@@ -1,3 +1,6 @@
+/**
+ * Middleware for dynamically generating sitemap.xml
+ */
 module.exports = function sitemap_xml(){
 	return function sitemap_xml(req, res, next){
 		console.log('sitemap xml handler');
