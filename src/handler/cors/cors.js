5b455d933349ff64a0f64ba2b212d4a2a4f8700c
kevinian <kevinpaul_boa@hotmail.com>
2013-09-11 21:25:28 +0200

    Update cors.js

--- src/handler/cors/cors.js
+++ src/handler/cors/cors.js
@@ -1,3 +1,8 @@
+/**
+ * Cross-Origin Resource Sharing
+ * http://www.w3.org/TR/cors/
+ * A mechanism to enable client-side cross-origin requests
+*/
 module.exports = function cors(){
 	return function cors(req, res, next){
 		console.log('cors handler');
